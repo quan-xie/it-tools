ff6cb29bc08677fdbc43793fcb45555e906cb9c3
xiequan <i@xiequan.info>
2023-09-06 03:34:55 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createHead } from '@vueuse/head';
 
+import ScriptX from 'vue-scriptx';
+import Ads from 'vue-google-adsense';
+
 import { registerSW } from 'virtual:pwa-register';
 import { plausible } from './plugins/plausible.plugin';
 
@@ -13,8 +16,7 @@ import App from './App.vue';
 import router from './router';
 import { i18nPlugin } from './plugins/i18n.plugin';
 
-import ScriptX from 'vue-scriptx'
-import Ads from 'vue-google-adsense'
+
 
 registerSW();
 
@@ -28,10 +30,10 @@ app.use(naive);
 app.use(plausible);
 
 
-app.use(ScriptX)
+app.use(ScriptX);
 
-app.use(Ads.Adsense)
-app.use(Ads.InArticleAdsense)
-app.use(Ads.InFeedAdsense)
+app.use(Ads.Adsense);
+app.use(Ads.InArticleAdsense);
+app.use(Ads.InFeedAdsense);
 
 app.mount('#app');
